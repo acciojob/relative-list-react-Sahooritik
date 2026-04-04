d6063656cebd349d87f9c6ac1ec6c6bbac4759a4
Ritik Sahoo <ritiksahoo380@gmail.com>
2026-04-04 18:57:46 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,12 @@ import React from 'react'
 const App = () => {
   return (
     <div id="main">
+        <ul>
+          <li>Bade Bhaiya</li>
+          <li>Chhote Bhaiya</li>
+          <li>Badi didi</li>
+          <li>Chhoti didi</li>
+        </ul>
                {/* Do not remove the main div */}
     </div>
   )
