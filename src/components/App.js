8642eead1e6a467db3c44621e37f9c1c7b9666c6
Ritik Sahoo <ritiksahoo380@gmail.com>
2026-04-19 14:36:30 +0530

    commit successful

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,10 @@ const App = () => {
   return (
     <div id="main">
         <ul>
-          <li>Bade Bhaiya</li>
-          <li>Chhote Bhaiya</li>
-          <li>Badi didi</li>
-          <li>Chhoti didi</li>
+          <li key='1'>Bade Bhaiya</li>
+          <li key='2'>Chhote Bhaiya</li>
+          <li key='3'>Badi didi</li>
+          <li key='4'>Chhoti didi</li>
         </ul>
                {/* Do not remove the main div */}
     </div>
